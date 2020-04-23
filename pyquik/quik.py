478e7c1a5402a0f3555daf02b4527ecffdb352cc
dezmond22 <dezmond22@yandex.ru>
2020-04-23 23:48:29 +0300

    Исправил ошибку

--- pyquik/quik.py
+++ pyquik/quik.py
@@ -48,12 +48,15 @@ class Quik:
         self.getRequest(cmd='IsSubscribed_Level_II_Quotes', data=self.secClass + '|' + toolCode)
         # Запрос: {"data":"CETS|USD000UTSTOM","id":14,"cmd":"IsSubscribed_Level_II_Quotes","t":1587660869588}
         self.getRequest(cmd='IsSubscribed_Level_II_Quotes', data=self.secClass + '|' + toolCode)
+        print('==========================\n==========================')
+        # Бесконечная подписка на стакан
         while (True):
             # Запрос: {"data": "CETS|USD000UTSTOM|LAST", "id": 11, "cmd": "getParamEx", "t": 1587660869568}
-            self.getRequest(cmd='getParamEx', data=self.clientCode + '|' + toolCode + '|' + 'LAST')
+            self.getRequest(cmd='getParamEx', data=self.secClass + '|' + toolCode + '|' + 'LAST')
             # Запрос: {"data":"MB0002500000|400K74Z|USD000UTSTOM|MB0002513352|2","id":12,"cmd":"getDepoEx","t":1587660869571}
             self.getRequest(cmd='getDepoEx', data=self.firmid + '|' + self.clientCode + '|'
                                                   + self.toolCode + '|' + self.tradeAccount + '|2')
+            time.sleep(1)
 
     # Создание инструмента
     def tool(self, toolCode):
