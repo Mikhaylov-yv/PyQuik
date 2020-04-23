28edf27d9f69eae89fb646ecad6c917091a2001b
dezmond22 <dezmond22@yandex.ru>
2020-04-23 23:07:47 +0300

    Сделал подписку на стакан котировок, но работает пока не очень...

--- pyquik/quik.py
+++ pyquik/quik.py
@@ -9,7 +9,7 @@ class Quik:
     port_callbacks = 34131
     host = "127.0.0.1"
     CRLF = "\r\n\r\n"
-    classCode = "SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB,CETS"
+    secClass_list = "SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB,CETS"
 
 
     def __init__(self):
@@ -17,8 +17,14 @@ class Quik:
         self.sok_callbacks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sok_callbacks.connect((self.host, self.port_callbacks))
         self.sok_requests.connect((self.host, self.port_requests))
-        self.clientCode = ''
+        # Используемые переменные
+        self.clientCode = '' # '400K74Z'
+        self.tradeAccount = '' # 'MB0002513352'
+        self.secClass = '' # 'CETS'
+        self.toolCode = ''  # 'USD000UTSTOM'
+        self.firmid = '' # 'MB0002500000'
         self.id = 0
+
 
     # Проверяет связь с программой
     def connekt(self):
@@ -27,17 +33,42 @@ class Quik:
                print('Подключение выполненно')
            # time.sleep(2)
 
+    def get_orderBook(self,toolCode):
+        self.toolCode = toolCode
+        self.tool(toolCode)
+        # Запрос: {"data":"CETS|USD000UTSTOM|SEC_PRICE_STEP","id":8,"cmd":"getParamEx","t":1587660869557}
+        self.getRequest(cmd='getParamEx',data = self.clientCode + '|' + toolCode + '|' + 'SEC_PRICE_STEP')
+        # Запрос: {"data": "CETS|USD000UTSTOM|SEC_SCALE", "id": 9, "cmd": "getParamEx", "t": 1587660869561}
+        self.getRequest(cmd='getParamEx',data = self.clientCode + '|' + toolCode + '|' + 'SEC_SCALE')
+        # Запрос: {"data":"CETS|USD000UTSTOM|LOTSIZE","id":10,"cmd":"getParamEx","t":1587660869562}
+        self.getRequest(cmd='getParamEx', data=self.clientCode + '|' + toolCode + '|' + 'LOTSIZE')
+        # Запрос: {"data":"CETS|USD000UTSTOM","id":13,"cmd":"Subscribe_Level_II_Quotes","t":1587660869576}
+        self.getRequest(cmd='Subscribe_Level_II_Quotes', data=self.secClass + '|' + toolCode )
+        # Запрос: {"data":"CETS|USD000UTSTOM","id":14,"cmd":"IsSubscribed_Level_II_Quotes","t":1587660869588}
+        self.getRequest(cmd='IsSubscribed_Level_II_Quotes', data=self.secClass + '|' + toolCode)
+        # Запрос: {"data":"CETS|USD000UTSTOM","id":14,"cmd":"IsSubscribed_Level_II_Quotes","t":1587660869588}
+        self.getRequest(cmd='IsSubscribed_Level_II_Quotes', data=self.secClass + '|' + toolCode)
+        while (True):
+            # Запрос: {"data": "CETS|USD000UTSTOM|LAST", "id": 11, "cmd": "getParamEx", "t": 1587660869568}
+            self.getRequest(cmd='getParamEx', data=self.clientCode + '|' + toolCode + '|' + 'LAST')
+            # Запрос: {"data":"MB0002500000|400K74Z|USD000UTSTOM|MB0002513352|2","id":12,"cmd":"getDepoEx","t":1587660869571}
+            self.getRequest(cmd='getDepoEx', data=self.firmid + '|' + self.clientCode + '|'
+                                                  + self.toolCode + '|' + self.tradeAccount + '|2')
+
     # Создание инструмента
-    def tool(self, secCode):
-        data = self.classCode + '|' + secCode
-        secClass = self.getRequest(cmd='getSecurityClass', data=data)['data']
+    def tool(self, toolCode):
+        data = self.secClass_list + '|' + toolCode
+        self.secClass = self.getRequest(cmd='getSecurityClass', data=data)['data']
         # Зачем нужен clientCode пока не понятно
-        self.clientCode = self.getRequest(cmd='getClientCode')
-        self.toll = self.getRequest(cmd = 'getSecurityInfo',data = str(secClass) + '|' + str(secCode))
+        self.clientCode = self.getRequest(cmd='getClientCode')['data']
+        self.toll = self.getRequest(cmd = 'getSecurityInfo', data = self.secClass + '|' + toolCode)['data']
+        self.tradeAccount = self.getRequest(cmd = 'getTradeAccount', data = self.secClass)['data']
+        self.toll_info = self.getRequest(cmd = 'getClassInfo',data = self.secClass)['data']
+        self.firmid = self.toll_info['firmid']
 
     # Определение secClass для создания инструмента
     def getSecurityClass(self, secCode):
-        data = self.classCode + '|' + secCode
+        data = self.secClass_list + '|' + secCode
         secClass = self.getRequest(cmd = 'getSecurityClass',data = data)['data']
         self.clientCode = self.getRequest(cmd = 'getClientCode')
         return secClass
@@ -50,7 +81,10 @@ class Quik:
         self.sok_requests.sendall((raw_data + self.CRLF).encode())
         while (True):
             response = self.sok_requests.recv(2048)
-            response = le(response.decode('ANSI')[:-1])
+            try:
+                response = le(response.decode('ANSI'))
+            except:
+                response = le(response.decode('ANSI').replace('true','"true"'))
             print('Запрос: ' + str(request) + '\n' + 'Ответ: ' + str(response))
             return response
 
@@ -58,7 +92,7 @@ class Quik:
 if __name__ == '__main__':
     q = Quik()
     q.connekt()
-    q.tool('USD000UTSTOM')
+    q.get_orderBook('USD000UTSTOM')
     # print(q.get_request('getSecurityClass', data =  'SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB,CETS|USD000000TOD'))
     # request = {"data":"SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB,CETS|USD000000TOD","id":3,"cmd":"getSecurityClass","t":0}
     # print(q.get_workingfolder(request))
