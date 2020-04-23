a1bd27fc85f686c91fa8f7751b6cf2ecb71bc790
dezmond22 <dezmond22@yandex.ru>
2020-04-23 20:23:49 +0300

    Создал инструмент

--- pyquik/quik.py
+++ pyquik/quik.py
@@ -2,12 +2,14 @@ import socket
 import json
 from datetime import datetime
 import time
+from ast import literal_eval as le
 
 class Quik:
     port_requests = 34130
     port_callbacks = 34131
     host = "127.0.0.1"
     CRLF = "\r\n\r\n"
+    classCode = "SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB,CETS"
 
 
     def __init__(self):
@@ -15,29 +17,48 @@ class Quik:
         self.sok_callbacks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sok_callbacks.connect((self.host, self.port_callbacks))
         self.sok_requests.connect((self.host, self.port_requests))
+        self.clientCode = ''
         self.id = 0
 
+    # Проверяет связь с программой
     def connekt(self):
-       if "lua_error" not in list(self.get_request('getWorkingFolder')):
-           self.get_request('isConnected')
-           time.sleep(2)
+       if "lua_error" not in list(self.getRequest('getWorkingFolder')):
+           if self.getRequest('isConnected')['cmd'] == 'isConnected':
+               print('Подключение выполненно')
+           # time.sleep(2)
 
-    def get_request(self, cmd, data = ''):
-        t = 0
+    # Создание инструмента
+    def tool(self, secCode):
+        data = self.classCode + '|' + secCode
+        secClass = self.getRequest(cmd='getSecurityClass', data=data)['data']
+        # Зачем нужен clientCode пока не понятно
+        self.clientCode = self.getRequest(cmd='getClientCode')
+        self.toll = self.getRequest(cmd = 'getSecurityInfo',data = str(secClass) + '|' + str(secCode))
+
+    # Определение secClass для создания инструмента
+    def getSecurityClass(self, secCode):
+        data = self.classCode + '|' + secCode
+        secClass = self.getRequest(cmd = 'getSecurityClass',data = data)['data']
+        self.clientCode = self.getRequest(cmd = 'getClientCode')
+        return secClass
+
+    # Метод для выполнения запроса к LUA скрипту
+    def getRequest(self, cmd, data = '', t = 0):
         self.id = self.id + 1
         request = {"data": data, "id": self.id, "cmd": cmd, "t": t}
         raw_data = json.dumps(request)
         self.sok_requests.sendall((raw_data + self.CRLF).encode())
         while (True):
             response = self.sok_requests.recv(2048)
-            print('Запрос: ' + str(request) + '\n' + 'Ответ: ' + str(response) )
+            response = le(response.decode('ANSI')[:-1])
+            print('Запрос: ' + str(request) + '\n' + 'Ответ: ' + str(response))
             return response
 
 
-
-
 if __name__ == '__main__':
-    q = Quik().connekt()
+    q = Quik()
+    q.connekt()
+    q.tool('USD000UTSTOM')
     # print(q.get_request('getSecurityClass', data =  'SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB,CETS|USD000000TOD'))
     # request = {"data":"SPBFUT,TQBR,TQBS,TQNL,TQLV,TQNE,TQOB,CETS|USD000000TOD","id":3,"cmd":"getSecurityClass","t":0}
     # print(q.get_workingfolder(request))
